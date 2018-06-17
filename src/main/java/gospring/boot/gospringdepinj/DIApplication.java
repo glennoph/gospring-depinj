35b71dad197474cdeb0710f0bed2505c3e075a5e
glenn opdycke-hansen <glennoph@gmail.com>
2018-06-17 08:27:23 -0500

    clean up code, replace println with log

--- src/main/java/gospring/boot/gospringdepinj/DIApplication.java
+++ src/main/java/gospring/boot/gospringdepinj/DIApplication.java
@@ -3,25 +3,28 @@ package gospring.boot.gospringdepinj;
 import gospring.boot.gospringdepinj.controllers.CtorInjectedController;
 import gospring.boot.gospringdepinj.controllers.MyController;
 import gospring.boot.gospringdepinj.controllers.SetterInjectedController;
-import gospring.boot.gospringdepinj.services.GreetingServiceImpl;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.apache.commons.logging.Log;
 
 @SpringBootApplication
 public class DIApplication {
 
 	public static void main(String[] args) {
-		System.out.println("* setup application context");
+		log.info("* setup application context");
 		ApplicationContext ctx = SpringApplication.run(DIApplication.class, args);
-		System.out.println("* get bean my controller");
+        log.info("* get bean my controller");
 		MyController myController = (MyController) ctx.getBean("myController");
 		myController.hello();
-		System.out.println("* ctor injector");
-		System.out.println(ctx.getBean(CtorInjectedController.class).sayHello());
-		System.out.println("* setter injection");
+        log.info("* ctor injector");
+        CtorInjectedController ctorInjectedController = ctx.getBean(CtorInjectedController.class);
+        log.info(ctorInjectedController.sayHello());
+        log.info("* setter injection");
 		SetterInjectedController setterInjectedController = ctx.getBean(SetterInjectedController.class);
-		//setterInjectedController.setGreetingService(new GreetingServiceImpl());
-		System.out.println(setterInjectedController.sayHello());
+        log.info(setterInjectedController.sayHello());
 	}
+
+	static Log log = LogFactory.getLog(DIApplication.class.getName());
 }
