9748b95d9c1e92be833a34cb62f865c0ecdabdf6
glenn opdycke-hansen <glennoph@gmail.com>
2018-06-16 22:21:43 -0500

    update spring config for ctor injection and setter injection

--- src/main/java/gospring/boot/gospringdepinj/DIApplication.java
+++ src/main/java/gospring/boot/gospringdepinj/DIApplication.java
@@ -1,6 +1,9 @@
 package gospring.boot.gospringdepinj;
 
+import gospring.boot.gospringdepinj.controllers.CtorInjectedController;
 import gospring.boot.gospringdepinj.controllers.MyController;
+import gospring.boot.gospringdepinj.controllers.SetterInjectedController;
+import gospring.boot.gospringdepinj.services.GreetingServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,8 +12,16 @@ import org.springframework.context.ApplicationContext;
 public class DIApplication {
 
 	public static void main(String[] args) {
+		System.out.println("* setup application context");
 		ApplicationContext ctx = SpringApplication.run(DIApplication.class, args);
+		System.out.println("* get bean my controller");
 		MyController myController = (MyController) ctx.getBean("myController");
 		myController.hello();
+		System.out.println("* ctor injector");
+		System.out.println(ctx.getBean(CtorInjectedController.class).sayHello());
+		System.out.println("* setter injection");
+		SetterInjectedController setterInjectedController = ctx.getBean(SetterInjectedController.class);
+		//setterInjectedController.setGreetingService(new GreetingServiceImpl());
+		System.out.println(setterInjectedController.sayHello());
 	}
 }
--- src/main/java/gospring/boot/gospringdepinj/controllers/CtorInjectedController.java
+++ src/main/java/gospring/boot/gospringdepinj/controllers/CtorInjectedController.java
@@ -1,10 +1,14 @@
 package gospring.boot.gospringdepinj.controllers;
 
 import gospring.boot.gospringdepinj.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class CtorInjectedController {
     private GreetingService greetingService;
 
+    @Autowired // nb this is optional for ctor injected
     public CtorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
